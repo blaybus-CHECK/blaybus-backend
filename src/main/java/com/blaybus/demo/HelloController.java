2185d8e9881c6438f8985a3d640b4b1fb6002aa6
SehyeonLee <tpgus2303@naver.com>
2025-08-08 13:31:28 +0900

    create projecdt

---
+++ src/main/java/com/blaybus/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.blaybus.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello, This is demo api server";
+    }
+}
