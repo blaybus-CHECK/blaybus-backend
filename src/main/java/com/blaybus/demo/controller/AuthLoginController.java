d92f80cf378251185fe75cd92626c46e49ae4cb7
SehyeonLee <tpgus2303@naver.com>
2025-08-20 16:53:08 +0900

    fix: 로그인/회원가입

--- src/main/java/com/blaybus/demo/controller/AuthLoginController.java
+++ src/main/java/com/blaybus/demo/controller/AuthLoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public record MemberIssueTokenController(
+public record AuthLoginController(
     MemberRepository repository,
     PasswordEncoder passwordEncoder,
     JwtKeyHolder jwtKeyHolder
--- src/main/java/com/blaybus/demo/controller/AuthRegisterController.java
+++ src/main/java/com/blaybus/demo/controller/AuthRegisterController.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 import static com.blaybus.demo.UserPropertyValidator.*;
 
 @RestController
-public record MemberSignUpController(
+public record AuthRegisterController(
     MemberRepository repository,
     PasswordEncoder passwordEncoder
 ) {
